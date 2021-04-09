a344759090322c6ee6b249fb7e7815a75cef4547
mohamad-amin <mohammadi.mohamadamin@gmail.com>
2021-04-08 18:37:17 -0700

    Added base class for being consistent with torch optimizers and a couple of sample algorithm implementations

---
+++ sag_linear_reg.py
@@ -0,0 +1,50 @@
+import torch
+import numpy as np
+from basic_saga import SAGA
+from torch.autograd import Variable
+from linear_regression import LinearRegression
+
+# create dummy data for training
+x_values = [i for i in range(11)]
+x_train = np.array(x_values, dtype=np.float32)
+x_train = x_train.reshape(-1, 1)
+
+y_values = [2*i + 1 for i in x_values]
+y_train = np.array(y_values, dtype=np.float32)
+y_train = y_train.reshape(-1, 1)
+
+inputDim = 1        # takes variable 'x'
+outputDim = 1       # takes variable 'y'
+learningRate = 0.01
+epochs = 100
+
+model = LinearRegression(inputDim, outputDim)
+
+criterion = torch.nn.MSELoss()
+# optimizer = torch.optim.SAG(model.parameters(), lr=learningRate)
+optimizer = SAGA(model.parameters(), N=x_train.shape[0], lr=learningRate)
+
+for epoch in range(epochs):
+    # Converting inputs and labels to Variable
+    i = np.random.choice(x_train.shape[0])
+    inputs = Variable(torch.from_numpy(x_train[i, :]))
+    labels = Variable(torch.from_numpy(y_train[i, :]))
+
+    # Clear gradient buffers because we don't want any gradient from previous epoch to carry forward, dont want to cummulate gradients
+    optimizer.zero_grad()
+
+    # get output from the model, given the inputs
+    outputs = model(inputs)
+
+    # get loss for the predicted output
+    loss = criterion(outputs, labels)
+    print(loss)
+    # get gradients w.r.t to parameters
+    loss.backward()
+
+    optimizer.set_step_information({'current_datapoint': i})
+
+    # update parameters
+    optimizer.step()
+
+    print('epoch {}, loss {}'.format(epoch, loss.item()))
