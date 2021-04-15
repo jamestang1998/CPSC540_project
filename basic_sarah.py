41eae3ea7f7553818ef9a1b77754e6b9f53a26cb
josef-w <645539308@qq.com>
2021-04-15 07:08:46 -0700

    SARAH-v3

--- basic_sarah.py
+++ basic_sarah.py
@@ -9,9 +9,9 @@ import torch
 from base_vr_optimizer import BaseVROptimizer
 
 
-class sarah(BaseVROptimizer):
+class SARAH(BaseVROptimizer):
 
-    def __init__(self, params, N, use_numba=False, lr=1e-2, eps=1e-6):
+    def __init__(self, params, N, batch_sizes,use_numba=False, lr=1e-2, eps=1e-6):
         self.N = N
         self.current_datapoint = -1
         self.passed_samples = 0
@@ -20,6 +20,7 @@ class sarah(BaseVROptimizer):
         #self.previous_parameters = []
         self.t = 10
         self.prev_full_grad = None
+        self.b=batch_sizes
         super().__init__(params, use_numba, lr, eps)
 
     def initialize_state(self, param, state):
@@ -29,49 +30,45 @@ class sarah(BaseVROptimizer):
 
     def _step(self, model_parameters, optimizer_parameters):
         lr = optimizer_parameters['lr']
-         
+        #print('######## vt-1 ######')
+        #print('vt-1',self.prev_full_grad)
         self.passed_samples += 1
+        #print('##### gradient of wt #####')
+        #print(model_parameters['grads'])
         for i in range(len(model_parameters['params'])):
             d_p = model_parameters['grads'][i]
         
             prev_d_p = self.previous_parameters[i]
 
-            self.prev_full_grad[i] = d_p - prev_d_p + self.prev_full_grad[i]
+            self.prev_full_grad[i] = (d_p - prev_d_p)/self.b + self.prev_full_grad[i]
 
-            model_parameters['params'][i].add_(self.prev_full_grad[i], alpha=-lr)
+            model_parameters['params'][i].data.add_(self.prev_full_grad[i], alpha=-lr)
+        #print('###### vt ######')
+        #print('vt',self.prev_full_grad)
         return
 
     def store_full_grad(self, layerList):
         self.prev_full_grad = []
         for i in range(len(layerList)):
-          self.prev_full_grad.append(layerList[i].grad)
-        #print(  self.param_groups[0]['params'][0])
-        #print(layerList[0])
-        #print(self.prev_full_grad)
+          self.prev_full_grad.append(layerList[i].grad/self.N)
         return
 
     def store_prev_grad(self, layerList):
         self.previous_parameters = []
         for i in range(len(layerList)):
           self.previous_parameters.append(layerList[i].grad)
+        #print(self.previous_parameters)
         return
     def _one_step_GD(self, model_parameters, optimizer_parameters):
         lr = optimizer_parameters['lr']
-
+        
         for i in range(len(model_parameters['params'])):
             #print(model_parameters['params'][i],i)
             #print(self.prev_full_grad[i])
-            model_parameters['params'][i]=model_parameters['params'][i].add(self.prev_full_grad[i], alpha=-lr)
+            model_parameters['params'][i].data.add_(self.prev_full_grad[i], alpha=-lr)
             #print(model_parameters['params'][i],i)
             pass
 
-    '''
-    def store_full_grad(self, model_parameters):
-        #print(model_parameters, "model_parameters !!!!!")
-        self.prev_full_grad = model_parameters['grads']
-        self.zero_grad()
-        return
-    '''
     def set_step_information(self, info_dict):
         if 'current_datapoint' in info_dict:
             self.current_datapoint = info_dict['current_datapoint']
--- sarah_linear_reg.py
+++ sarah_linear_reg.py
@@ -7,7 +7,7 @@ Created on Tue Apr 13 01:51:48 2021
 """
 import torch
 import numpy as np
-from basic_sarah import sarah
+from basic_sarah import SARAH
 from torch.autograd import Variable
 from linear_regression import LinearRegression
 
@@ -27,21 +27,26 @@ y_train = y_train.reshape(-1, 1)
 inputDim = 1        # takes variable 'x'
 outputDim = 1       # takes variable 'y'
 learningRate = 0.01
-epochs = 1
+batch_sizes=5
+if batch_sizes > x_train.shape[0]:
+    print('Warning! You are trying to assign batch greater than number of data.')
+    batch_sizes = x_train.shape[0]
+epochs = 20
 
 model = LinearRegression(inputDim, outputDim)
-model_checkpoint = LinearRegression(inputDim, outputDim)
+model_checkpoint = copy.deepcopy(model)
+
 
 criterion = torch.nn.MSELoss()
 # optimizer = torch.optim.SAG(model.parameters(), lr=learningRate)
-optimizer = sarah(model.parameters(), N=x_train.shape[0], lr=learningRate)
-optimizer1 = sarah(model_checkpoint.parameters(), N=x_train.shape[0], lr=learningRate)
+optimizer = SARAH(model.parameters(), N=x_train.shape[0],batch_sizes=batch_sizes,lr=learningRate)
+#optimizer1 = sarah(model_checkpoint.parameters(), N=x_train.shape[0], lr=learningRate)
 
 for epoch in range(epochs):
     # Converting inputs and labels to Variable
 
     # Clear gradient buffers because we don't want any gradient from previous epoch to carry forward, dont want to cummulate gradients
-    optimizer.zero_grad()
+    model.zero_grad()
     model_checkpoint.zero_grad()
     # get output from the model, given the inputs
     for i in range(x_train.shape[0]):
@@ -51,43 +56,58 @@ for epoch in range(epochs):
       outputs1 = model_checkpoint(inputs1)
       loss1 = criterion(outputs1, labels1)
       loss1.backward()
-
+   
     #optimizer.step()
     optimizer.store_full_grad(list(model_checkpoint.parameters()))
-    #update for the first step
+    model_checkpoint.zero_grad() 
+    #print('###### w0 we want ######')
+    #print(list(model_checkpoint.parameters()))
+    #using GD to update for the first step: w_1
+    #print('#######one step GD########')
     optimizer.one_step_GD()
+    #print('####### w1 #########')
+    #print(list(model.parameters()))
     m = np.random.choice(x_train.shape[0])
     if m==0:
         m=1
-    #store
+    #print('##########inner loop#########')
     for i in range(m):
       ##############################
-      j = np.random.choice(m)
-      inputs = Variable(torch.from_numpy(x_train[j, :]))
-      labels = Variable(torch.from_numpy(y_train[j, :]))
-
+      #print('######### i={} #########'.format(i))
       # Clear gradient buffers because we don't want any gradient from previous epoch to carry forward, dont want to cummulate gradients
       model.zero_grad()
       model_checkpoint.zero_grad()
-      # get output from the model, given the inputs
-      outputs = model(inputs)
-      outputs1 = model_checkpoint(inputs)
-
-      # get loss for the predicted output
-      loss = criterion(outputs, labels)
-      loss1 = criterion(outputs1, labels)
-
-      #print(loss)
-      # get gradients w.r.t to parameters
-      loss.backward()
-      loss1.backward()
-
-      optimizer.store_prev_grad(list(model.parameters()))
-      print('v{}k'.format(i),list(model.parameters()))
-
-      optimizer.set_step_information({'current_datapoint': j})
+      
+      batch=np.random.choice(a=x_train.shape[0], size=batch_sizes, replace=False)
+      for j in batch:
+          inputs = Variable(torch.from_numpy(x_train[j, :]))
+          labels = Variable(torch.from_numpy(y_train[j, :]))
+    
+          # get output from the model, given the inputs
+          outputs = model(inputs)
+          outputs1 = model_checkpoint(inputs)
+    
+          # get loss for the predicted output
+          loss = criterion(outputs, labels)
+          loss1 = criterion(outputs1, labels)
+    
+          # get gradients w.r.t to parameters
+          loss.backward()
+          loss1.backward()
+      
+      
+      #print('######### w_{} ##########'.format(i))
+      #print('w{}'.format(i),list(model.parameters()))
+      
+      #print('######### gradient of w_{} ##########'.format(i-1))
+      optimizer.store_prev_grad(list(model_checkpoint.parameters()))
+      model_checkpoint =  copy.deepcopy(model)
       # update parameters
       optimizer.step()
+      
+      #print('######### w_{} ##########'.format(i+1))
+      #print('w{}'.format(i+1),list(model.parameters()))
+      
     model_checkpoint =  copy.deepcopy(model)
     
     print('epoch {}, loss {}'.format(epoch, loss.item()))
