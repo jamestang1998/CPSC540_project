66c625f411c86de261d47f58085f362a1cd41dc4
James Tang <tangytoby@yahoo.com>
2021-04-21 20:39:12 -0700

    changed some directories

--- main.py
+++ main.py
@@ -101,12 +101,12 @@ if __name__ == "__main__":
                     [transforms.ToTensor(),
                     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-        trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+        trainset = torchvision.datasets.CIFAR10(root='./local/tangytob/CPSC540/data', train=True,
                                         download=True, transform=transform)
         trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
                                                 shuffle=True, num_workers=0)
 
-        testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+        testset = torchvision.datasets.CIFAR10(root='./local/tangytob/CPSC540/data', train=False,
                                             download=True, transform=transform)
         testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
                                                 shuffle=False, num_workers=num_workers)
@@ -114,12 +114,12 @@ if __name__ == "__main__":
     else: # it's MNIST
         transform = transforms.Compose(
                     [transforms.ToTensor()])
-        trainset = torchvision.datasets.MNIST(root='./data', train=True,
+        trainset = torchvision.datasets.MNIST(root='./local/tangytob/CPSC540/data', train=True,
                                         download=True, transform=transform)
         trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
                                                 shuffle=True, num_workers=0)
 
-        testset = torchvision.datasets.MNIST(root='./data', train=False,
+        testset = torchvision.datasets.MNIST(root='./local/tangytob/CPSC540/data', train=False,
                                             download=True, transform=transform)
         testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
                                                 shuffle=False, num_workers=num_workers)
